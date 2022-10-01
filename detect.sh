97bb06a3882e3ab9a2e29654402e3a579e1f0a6b
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-10-01 12:20:21 +0530

    Bash Script To Detect Text Files, C++ files, C files, Shell Script files, etc

--- detect.sh
+++ detect.sh
@@ -3,7 +3,19 @@
 
 if [ ${1##*.} == "txt" ]
 then
-	echo "Text file!!"
+	echo "Text file"
+
+elif [ ${1##*.} == "sh" ]
+then
+	echo "Bash Script"
+
+elif [ ${1##*.} == "cpp" ]
+then
+	echo "C++ source file"
+
+elif [ ${1##*.} == "c" ]
+then
+	echo "C source file"
 
 else
 	echo "Not sure!!"
